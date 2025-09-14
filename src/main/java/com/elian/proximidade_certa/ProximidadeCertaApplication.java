b7bea09779ff1a6fd3341f08dcf87790b2206d45
Elian-beep <elian.19batista@gmail.com>
2025-09-14 00:46:22 -0400

    chore: config db

---
+++ src/main/java/com/elian/proximidade_certa/ProximidadeCertaApplication.java
@@ -0,0 +1,13 @@
+package com.elian.proximidade_certa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximidadeCertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximidadeCertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elian/proximidade_certa/ProximidadeCertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elian.proximidade_certa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximidadeCertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
